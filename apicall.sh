f2629cf3e23ef246f5949f299b3f1e9910b3ffbd
Molnár Imre <166641267+molnarimi0211@users.noreply.github.com>
2025-04-29 10:17:20 +0200

    Update apicall.sh

--- apicall.sh
+++ apicall.sh
@@ -4,5 +4,8 @@
 
 api='https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/all'
 
+# api='https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/by-cca3'
+# curl -s "$api/$1" | jq -r '.name.official'
+
 
 curl -s "$api" | jq -r '.[] | "\(.name.official) - \(.cca3)"'
